ff2854c4449e36d74a1a38c85c8123d30b80f096
vxrshxx <124232595+vxrshxx@users.noreply.github.com>
2023-03-17 15:48:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import World from './Components/World';
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+      <World/>
+      
+      </div>
+     
+  );
+}
+
+export default App;
---
+++ World.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function World() {
+  return (
+    <div><p>This is Functional Component</p></div>
+  )
+}
+
+export default World
